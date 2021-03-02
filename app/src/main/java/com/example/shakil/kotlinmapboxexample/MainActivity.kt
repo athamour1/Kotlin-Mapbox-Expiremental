38ad80e59fa12d71c6ebeff1dd7dc2f0b661cb09
athamour1 <thanosmour.tk@gmail.com>
2021-03-02 10:39:22 +0200

    add pins and clear app

---
+++ app/src/main/java/com/example/shakil/kotlinmapboxexample/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.example.shakil.kotlinmapboxexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
+
+    private var mapboxMap: MapboxMap? = null
+    private var permissionsManager: PermissionsManager? = null
+    private var locationComponent: LocationComponent? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Mapbox.getInstance(this, getString(R.string.access_token))
+        setContentView(R.layout.activity_main)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(getString(R.string.navigation_guidance_day)) {
+            style: Style? ->
+            enableLocationComponent(style)
+        }
+        mapboxMap?.addMarker(
+            MarkerOptions()
+            .position(LatLng(40.51019146840461, 21.269610214589584))
+            .title("H limnh"))
+    }
+
+    private fun enableLocationComponent(loadedMapStyle: Style?) {
+        //Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Activity the MapboxMap LocationComponent to show user location
+            // Adding in LocationComponentOptions is also an optional parameter
+            locationComponent = mapboxMap!!.locationComponent
+            locationComponent!!.activateLocationComponent(this, loadedMapStyle!!)
+            locationComponent!!.setLocationComponentEnabled(true)
+
+            //Set the component's camera mode
+            locationComponent!!.setCameraMode(CameraMode.TRACKING)
+        }
+
+        else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager!!.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocationComponent(mapboxMap!!.style)
+        }
+        else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionsManager!!.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
+}
